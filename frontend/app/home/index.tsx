f6606443d7f2a3762d895f3454128762d1d1d9db
Matheus Rocha <im.rochamatheus@gmail.com>
2024-04-07 18:41:57 -0300

    :sparkles: feat: adicionar estrutura inicial para página home quando usuário recrutador

--- frontend/app/home/index.tsx
+++ frontend/app/home/index.tsx
@@ -1,9 +1,48 @@
-import { Text, View } from "react-native";
+import DefaultButton from "@/components/DefaultButton";
+import { FontSize, Spacing } from "@/constants/Sizes";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 
 export default function Home() {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <View style={styles.buttonWrapper}>
+        <DefaultButton title="Cadastrar nova vaga"></DefaultButton>
+      </View>
+      <View style={styles.jobsContainer}>
+        <FlatList
+          ListHeaderComponent={
+            <Text style={styles.jobsContainerTitle}>Suas vagas</Text>
+          }
+          data={Array(10)}
+          renderItem={({ index }) => (
+            <View style={{ padding: Spacing.small }}>
+              <Text>Componente card de vaga {index}</Text>
+            </View>
+          )}
+        />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    marginTop: Spacing.large,
+  },
+  buttonWrapper: {
+    width: "60%",
+  },
+  jobsContainer: {
+    width: "100%",
+    padding: Spacing.smallMedium,
+  },
+  jobsContainerTitle: {
+    fontSize: FontSize.mediunLarge,
+    marginTop: Spacing.smallMedium,
+    fontFamily: "Roboto-Regular",
+  },
+});
