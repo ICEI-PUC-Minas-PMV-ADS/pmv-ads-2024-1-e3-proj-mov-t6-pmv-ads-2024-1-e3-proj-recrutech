1a530af8f2101322ba31292c20224bf94860d679
Matheus Rocha <im.rochamatheus@gmail.com>
2024-04-07 09:49:59 -0300

    :sparkles: feat: adicionar componente de header para página principal

---
+++ frontend/components/HeaderComponent.tsx
@@ -0,0 +1,52 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+
+import AppTitle from "./AppTitle";
+
+import { Spacing } from "@/constants/Sizes";
+import { Colors } from "@/constants/Colors";
+
+export default function HeaderComponent() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.nav}>
+        <View>
+          <AppTitle size={Spacing.large}></AppTitle>
+          <Text style={styles.titleSuffix}>for recruiters</Text>
+        </View>
+        <View>
+          <Image source={require("@/assets/images/profile.png")} />
+        </View>
+      </View>
+
+      <Text style={styles.greetingText}>
+        <Text style={styles.textGreen}>Bem vindo,</Text> candidato!
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    justifyContent: "space-between",
+  },
+  nav: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  titleSuffix: {
+    alignSelf: "flex-end",
+    fontFamily: "Roboto-Bold",
+    fontSize: Spacing.smallMedium,
+  },
+  greetingText: {
+    fontSize: Spacing.medium,
+    fontFamily: "Roboto-Bold",
+    marginTop: Spacing.large,
+    paddingLeft: Spacing.small,
+  },
+  textGreen: {
+    color: Colors.green,
+  },
+});
