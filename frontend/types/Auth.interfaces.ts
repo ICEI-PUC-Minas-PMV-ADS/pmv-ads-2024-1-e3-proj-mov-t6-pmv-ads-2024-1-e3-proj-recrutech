e86db84fd648422c14cc5a20b23b356b35059ad3
Matheus Rocha <im.rochamatheus@gmail.com>
2024-04-16 13:52:09 -0300

    :label: refactor: adicionar arquivo separado para conter interfaces de autenticação

---
+++ frontend/types/Auth.interfaces.ts
@@ -0,0 +1,24 @@
+export namespace AuthInterfaces {
+  export interface Send {
+    email: string;
+    password: string;
+  }
+
+  export interface Receive {
+    token: string;
+  }
+}
+
+export interface AuthService {
+  signIn: (data: AuthInterfaces.Send) => Promise<AuthInterfaces.Receive | void>;
+  signOut: () => void;
+}
+
+export interface RenderTextFieldProps {
+  field: any;
+  label: string;
+  error?: string;
+  placeholder: string;
+  secureTextEntry?: boolean;
+  variant: "primary" | "secondary" | undefined;
+}
