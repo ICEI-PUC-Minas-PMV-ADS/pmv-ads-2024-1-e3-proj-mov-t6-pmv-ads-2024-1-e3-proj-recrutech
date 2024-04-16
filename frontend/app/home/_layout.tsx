628cf8038f67f166e364d435bef41f6e9777bb38
Matheus Rocha <im.rochamatheus@gmail.com>
2024-04-16 13:53:51 -0300

    :sparkles: feat: adicionar login de usuário

--- frontend/app/home/_layout.tsx
+++ frontend/app/home/_layout.tsx
@@ -4,9 +4,13 @@ import { Slot } from "expo-router";
 
 import { Spacing } from "@/constants/Sizes";
 
+import { useSession } from "@/context/AuthContext";
+
 import HeaderComponent from "@/components/HeaderComponent";
 
 export default function HomeLayout() {
+  const { session, isLoading, signIn } = useSession();
+
   return (
     <View style={styles.container}>
       <HeaderComponent />
