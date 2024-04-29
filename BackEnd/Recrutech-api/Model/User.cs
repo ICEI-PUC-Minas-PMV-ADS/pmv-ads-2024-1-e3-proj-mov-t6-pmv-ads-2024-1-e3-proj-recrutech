a4726a146469bee3f632e7aa70226b6dfa0eddb6
VitorCoS <vitor_cos12@yahoo.com.br>
2024-04-29 20:45:44 -0300

    t1

--- BackEnd/Recrutech-api/Model/User.cs
+++ BackEnd/Recrutech-api/Model/User.cs
@@ -11,6 +11,7 @@ namespace Recrutech_api.Model
         public bool? IsRecruiter { get; set; }
         public long? AddressId { get; set; }
         public Curriculum? Curriculum { get; set; }
+
         public Address? Address { get; set; }
         public List<Vacancy>? VacanciesOwner { get; set; }
         public List<UserRecommendation>? UserRecommendations { get; set; }
