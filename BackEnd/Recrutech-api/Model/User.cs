9f7c15ab58a43fdd3f864ab0a08b590538244395
VitorCoS <vitor_cos12@yahoo.com.br>
2024-05-01 20:46:52 -0300

    database adjust

--- BackEnd/Recrutech-api/Model/User.cs
+++ BackEnd/Recrutech-api/Model/User.cs
@@ -12,7 +12,6 @@ namespace Recrutech_api.Model
         public string? Password { get; set; }
         public string? Email { get; set; }
         public bool? IsRecruiter { get; set; }
-        public long? AddressId { get; set; }
         public Curriculum? Curriculum { get; set; }
         public Address? Address { get; set; }
         public List<Vacancy>? VacanciesOwner { get; set; }
