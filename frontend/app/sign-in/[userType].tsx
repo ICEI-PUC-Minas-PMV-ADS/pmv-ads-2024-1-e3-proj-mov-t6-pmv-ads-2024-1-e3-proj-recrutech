bb1e052eb8824ae552b9e281700e781d41047090
sandromaciel <sandros.maciel@gmail.com>
2024-05-25 22:44:10 -0300

    :sparkles: feat: remove usuário de login padrão

--- frontend/app/sign-in/[userType].tsx
+++ frontend/app/sign-in/[userType].tsx
@@ -51,10 +51,7 @@ export default function Login() {
   const fieldVariant = getFieldVariantByUser(userType);
   const buttonVariant = getButtonVariantByUser(userType);
 
-  const defaultValues: AuthInterfaces.Send = {
-    email: "sandro.maciel@mail.com",
-    password: "123456",
-  };
+  const defaultValues: AuthInterfaces.Send = {} as AuthInterfaces.Send;
 
   const {
     control,
